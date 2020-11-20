bf8d8c5830aaeb76a3a6b8239e867537c28851b9
Thomas Chen <zhejianchen2019@u.northwestern.edu>
2020-11-20 11:07:36 -0600

    Last checkpoint in Chicago

--- helper.py
+++ helper.py
@@ -3,7 +3,7 @@ from urllib.request import urlopen
 import re
 import pandas as pd
 import sqlalchemy as db
-
+from tqdm import tqdm
 
 def get_historical_links(stock, minYear):
 	start = 0
@@ -26,7 +26,7 @@ def get_historical_links(stock, minYear):
 	return links
 
 def find_all_reports_today():
-    html = urlopen("https://www.sec.gov/cgi-bin/current?q1=0&q2=0&q3=4") 
+    html = urlopen("https://www.sec.gov/cgi-bin/current?q1=0&q2=0&q3=4")
     bsObj = BeautifulSoup(html.read())
     nameList = bsObj.findAll('a')
     reports_link = []
@@ -43,7 +43,7 @@ def find_all_xml(reports_web_link):
         url = 'https://sec.gov'+link
         html = urlopen(url)
         bsObj = BeautifulSoup(html.read())
-        nameList = bsObj.findAll('a') 
+        nameList = bsObj.findAll('a')
         for link in nameList:
             filename = link.text
             if 'xml' in filename.split('.'):
@@ -54,35 +54,32 @@ def find_all_xml(reports_web_link):
 def get_filing_data(xml_reports_link):
     all_trans = []
     ct = 0
-    for xml in xml_reports_link:
+    for xml in tqdm(xml_reports_link, position=0, leave=True):
         url = "https://sec.gov" + xml
         html = urlopen(url)
         bsObj = BeautifulSoup(html.read())
-        symbol = bsObj.issuertradingsymbol.text
-        name = bsObj.reportingowner.reportingownerid.rptownername.text
-        owner = bsObj.reportingownerrelationship
-        # person_type_map = {'0010': 'major holder',
-        #                    '0100': 'officer',
-        #                    '1000': 'director',
-        #                    '0001': 'other',
-        #                    'unknown': None}
-        try:
-            title = owner.officertitle.text
-        except:
-            print("Erorr Finding Insider Position")
-            title = ''
         stock_trans = bsObj.findAll('nonderivativetransaction')
         if len(stock_trans) == 0:
         	print("No stock transactions")
         	continue
+
+        symbol = bsObj.issuertradingsymbol.text
+        name = bsObj.reportingowner.reportingownerid.rptownername.text
+        owner = bsObj.reportingownerrelationship
+        try:
+            title = owner.officertitle.text
+        except:
+            #print("Erorr Finding Insider Position")
+            title = ''
+
         for trans in stock_trans:
             try:
                 trans_type = trans.transactioncode.text
             except:
-                print("Error Finding Transaction Type")
-                trans_type = None
+                #print("Error Finding Transaction Type")
+                trans_type = ''
             try:
-            	all_trans.append({'ticker':symbol, 
+            	all_trans.append({'ticker':symbol,
             		'trans_date':trans.transactiondate.value.string,
             		'price':float(trans.transactionpricepershare.value.string),
             		'num_shares':int(float(trans.transactionshares.value.string)),
@@ -90,14 +87,10 @@ def get_filing_data(xml_reports_link):
             		'trans_type': trans_type,
             		'name':name,
             		'position':title,
-                              # 'isdirector': owner.isdirector.text,
-                              # 'isofficer': owner.isofficer.text,
-                              # 'ismajor':owner.istenpercentowner.text,
-                              # 'isother': owner.isother.text,
-                              'owned_shares': int(float(trans.sharesownedfollowingtransaction.value.string)),})
+                    'owned_shares': int(float(trans.sharesownedfollowingtransaction.value.string)),})
             	ct = ct + 1
             except Exception as e:
-            	print("Error {} occured for getting filing data for {}".format(e, symbol))
+            	print("Error {} occured for getting transaction for {} with transaction id = {}".format(e, symbol, ct))
             	print(url)
     print("Total Number of transactions ", ct)
     df = pd.DataFrame(all_trans)
--- setup_insider.py
+++ setup_insider.py
@@ -8,7 +8,11 @@ import re
 if __name__ == '__main__':
 	con = db.create_engine("sqlite:///C:/Users/zheji/Desktop/TradingTools/fundamentalDB.sqlite")
 	stocks = pd.read_csv('constituents_csv.csv').Ticker.unique()
-	for stock in stocks:
+	for stock in stocks[8:]:
+		check = stock.split('.')
+		if len(check)!= 1:
+			print("Can't get info for ", stock)
+			continue
 		print("Getting insider transactions for ", stock)
 		reports = scraper.get_historical_links(stock, 15)
 		xml = scraper.find_all_xml(reports)
